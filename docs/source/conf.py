cb9f046e855c33d32a483e3d891dfb4e61f6b834
albertbou92 <albertbo@kth.se>
2020-12-31 13:21:52 +0100

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../pytorchrl/'))
+sys.path.insert(0, os.path.abspath('../../../pytorchrl/'))
 
 # -- Project information -----------------------------------------------------
 
