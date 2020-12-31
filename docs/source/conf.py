cc7435456d80d30caa181d1c1f866dc5f1016429
albertbou92 <albertbo@kth.se>
2020-12-31 13:12:50 +0100

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../../pytorchrl/'))
+sys.path.insert(0, os.path.abspath('../../pytorchrl/'))
 
 # -- Project information -----------------------------------------------------
 
@@ -29,9 +29,6 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-
-master_doc = 'index'
-html_static_path = []
 
 extensions = [
     'sphinx.ext.autodoc',
