512909040c02a4e0ab2808131835729f746aa04c
albert bou <albertbou92@gmail.com>
2021-09-23 13:21:49 +0200

    to merge

--- code_examples/train_animalai/ppod/enjoy.py
+++ code_examples/train_animalai/ppod/enjoy.py
@@ -2,9 +2,10 @@
 
 import os
 import torch
-import argparse
-from pytorchrl.envs import pybullet_test_env_factory
-from pytorchrl.agent.actors import OnPolicyActor, get_feature_extractor
+import pytorchrl as prl
+from pytorchrl.agent.env import VecEnv
+from pytorchrl.agent.actors import OnPolicyActor
+from pytorchrl.envs.animal_olympics.animal_olympics_env_factory import animal_train_env_factory
 from code_examples.train_animalai.ppod.train import get_args
 
 
@@ -13,34 +14,43 @@ def enjoy():
     args = get_args()
 
     # Define single copy of the environment
-    env = pybullet_test_env_factory(env_id=args.env_id)
-    env.render()
+    arena_file = os.path.dirname(os.path.abspath(__file__)) + "/arenas/"
+    env, action_space, obs_space = VecEnv.create_factory(
+        env_fn=animal_train_env_factory,
+        env_kwargs={
+            "arenas_dir": arena_file,
+            "frame_skip": args.frame_skip,
+            "frame_stack": args.frame_stack,
+            "inference": True,
+        }, vec_env_size=1)
 
-    # Define agent device and agent
+    # Define agent device and policy
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
     policy = OnPolicyActor.create_factory(
-        env.observation_space, env.action_space,
-        restart_model=os.path.join(args.log_dir, "model.state_dict"))(device)
+        obs_space, action_space, prl.PPO,
+        restart_model=args.restart_model,
+        recurrent_nets=args.recurrent_nets)(device)
 
     # Define initial Tensors
-    obs, done = env.reset(), False
+    env = env()
+    obs = env.reset()
+    done, episode_reward, step = False, 0, 0
     _, rhs, _ = policy.actor_initial_states(torch.tensor(obs))
-    episode_reward = 0
 
     # Execute episodes
     while not done:
 
-        env.render()
-        obs = torch.Tensor(obs).view(1, -1).to(device)
-        done = torch.Tensor([done]).view(1, -1).to(device)
+        obs = torch.Tensor(obs).to(device)
+        done = torch.Tensor([done]).to(device)
+
         with torch.no_grad():
-            _, clipped_action, _, rhs, _ = policy.get_action(obs, rhs, done, deterministic=True)
+            _, clipped_action, _, rhs, _, _ = policy.get_action(obs, rhs, done, deterministic=True)
+
         obs, reward, done, info = env.step(clipped_action.squeeze().cpu().numpy())
         episode_reward += reward
 
         if done:
-            print("EPISODE: reward: {}".format(episode_reward), flush=True)
+            print("EPISODE: reward: {}".format(episode_reward.item()), flush=True)
             done, episode_reward = 0, False
             obs = env.reset()
 
--- pytorchrl/__init__.py
+++ pytorchrl/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.3.40"
+__version__ = "1.3.50"
 
 from collections import namedtuple
 
