36b3677d9fd467f6b1c003a8cc296c53fdc45fb9
albert bou <albertbou92@gmail.com>
2021-09-29 11:54:34 +0200

    architectures

--- pytorchrl/agent/actors/feature_extractors/cnn.py
+++ pytorchrl/agent/actors/feature_extractors/cnn.py
@@ -59,7 +59,7 @@ class CNN(nn.Module):
 
         # Define final layer
         feature_size = int(np.prod(self.feature_extractor(
-            torch.randn(1, *self.input_space.shape)).shape))
+            torch.randn(1, *input_space.shape)).shape))
         self.head = nn.Sequential(
             nn.Linear(feature_size, output_size),
             activation())
