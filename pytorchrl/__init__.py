cba36b63939c42005e2941fd1b21ef40051359e9
albert bou <albertbou92@gmail.com>
2022-03-21 12:23:38 +0100

    minor fix

--- pytorchrl/__init__.py
+++ pytorchrl/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2.1.66"
+__version__ = "2.1.67"
 
 from collections import namedtuple
 
--- pytorchrl/scheme/scheme.py
+++ pytorchrl/scheme/scheme.py
@@ -1,3 +1,6 @@
+import logging
+logger = logging.getLogger(__name__)
+
 import pytorchrl as prl
 from pytorchrl.scheme.collection.c_worker_set import CWorkerSet
 from pytorchrl.scheme.gradients.g_worker_set import GWorkerSet
@@ -125,6 +128,8 @@ class Scheme:
             local_device=local_device,
             decentralized_update_execution=decentralized_update_execution,
         )
+
+        logger.warning("Created training scheme.")
 
     def update_worker(self):
         """Return local worker"""
