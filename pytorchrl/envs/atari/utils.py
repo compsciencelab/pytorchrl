3c9fe29a043b8e847067fa489bff9fa0326bdbc5
albert bou <albertbou92@gmail.com>
2022-05-17 10:42:43 +0200

    atari emdebs

--- pytorchrl/envs/atari/utils.py
+++ pytorchrl/envs/atari/utils.py
@@ -7,5 +7,5 @@ def imdownscale(state, target_shape=(11, 8), max_pix_value=8):
         resized = state
     else:
         resized = cv2.resize(state, target_shape, interpolation=cv2.INTER_AREA)
-    img = ((resized / 255.0) * max_pix_value).astype(np.uint8)
+    img = ((resized / 255.0) * max_pix_value).astype(np.int8)
     return img
