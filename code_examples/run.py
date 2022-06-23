38623defcf021917da2938c40095f367f4a35be8
BY571 <sebastian.dittert@gmx.de>
2022-06-23 12:10:15 +0200

    update code examples

--- code_examples/run.py
+++ code_examples/run.py
@@ -1,13 +1,15 @@
 from omegaconf import DictConfig, OmegaConf
 import hydra
+from pytorchrl.trainer import Trainer
 
-from pytorchrl.custom_environments import my_custom_environment_factory
+# from pytorchrl.custom_environments import my_custom_environment_factory
 
 @hydra.main(config_name="conf", config_path="./cfg")
 def run_training(cfg: DictConfig) -> None:
     config = OmegaConf.to_yaml(cfg)
-    
-    trainer = Trainer(config, my_custom_environment_factory)
+    print("Start Training\n")
+    print("Training config: ", config, "\n")
+    trainer = Trainer(cfg, custom_environment_factory=None)
     trainer.train()
 
 
--- pytorchrl/trainer.py
+++ pytorchrl/trainer.py
@@ -1,37 +1,68 @@
+import os
+import sys
+import time
 
 from pytorchrl.agent.env import VecEnv
 from pytorchrl.learner import Learner
 from pytorchrl.scheme import Scheme
+from pytorchrl.utils import save_argparse, cleanup_log_dir
 
 ON_POLICY_ALGOS = ["PPO", "PPOD"]
 OFF_POLICY_ALGOS = ["DQN", "DDPG", "TD3", "SAC"]
 
 class Trainer():
     def __init__(self, config, custom_environment_factory=None):
+        print(config)
+        cleanup_log_dir(config.log_dir)
+        # not sure if this is needed anymore since we store it anyway on wandb
+        # save_argparse(config, os.path.join(config.log_dir, "conf.yaml"),[])
         self.config = config
         self.custom_environment_factory = custom_environment_factory
         
         self.train_envs_factory, self.test_envs_factory, self.action_space, self.obs_space = self.setup_environment()
         self.algo_factory, self.algo_name = get_algorithm(self.config)
-        self.algo_factory = self.setup_algorithm()
+        self.actor_factory = self.setup_algorithm()
+        self.storage_factory = self.setup_storage()
+        self.scheme = self.setup_scheme()
+
         
-        
+    def train(self, ):
+        iterations = 0
+        start_time = time.time()
+        while not self.learner.done():
+
+            self.learner.step()
+
+            if iterations % self.config.log_interval == 0:
+                self.learner.print_info()
+
+            if iterations % self.config.save_interval == 0:
+                save_name = self.learner.save_model()
+
+            if self.config.max_time != -1 and (time.time() - start_time) > self.config.max_time:
+                break
+
+            iterations += 1
+
+        print("Finished!")
+        sys.exit()
 
     def setup_environment(self,):
         if self.custom_environment_factory is not None:
             environment_train_factory = self.custom_environment_factory
         else:
-            environment_train_factory, environment_test_factory = get_enviroment_factory(self.config.task)
+            environment_train_factory, environment_test_factory = self.get_enviroment_factory(self.config.environment)
         # 1. Define Train Vector of Envs
+
         train_envs_factory, action_space, obs_space = VecEnv.create_factory(
-            env_fn=self.environment_train_factory,
-            env_kwargs={self.config.task},
+            env_fn=environment_train_factory,
+            env_kwargs=self.config.task,
             vec_env_size=self.config.num_env_processes, log_dir=self.config.log_dir)
 
         # 2. Define Test Vector of Envs (Optional)
         test_envs_factory, _, _ = VecEnv.create_factory(
-            env_fn=self.environment_test_factory,
-            env_kwargs={self.config.task},
+            env_fn=environment_test_factory,
+            env_kwargs=self.config.task,
             vec_env_size=self.config.num_env_processes, log_dir=self.config.log_dir)
         
         return train_envs_factory, test_envs_factory, action_space, obs_space
@@ -51,23 +82,57 @@ class Trainer():
                                                           self.action_space,
                                                           self.algo_name,
                                                           restart_model=self.config.restart_model,
-                                                          noise=self.config.agent.noise)
+                                                          noise=self.config.agent.ddpg_config.noise)
             return actor_factory
+        else:
+            pass
+        
+    def setup_storage(self,):
+        print(self.config.agent)
+        if self.config.agent.storage.name == "replay_buffer":
+            from pytorchrl.agent.storages import ReplayBuffer
+            return  ReplayBuffer.create_factory(size=self.config.agent.storage.size)
+        elif self.config.agent.storage.name == "her_buffer":
+            from pytorchrl.agent.storages import HERBuffer
+            return HERBuffer.create_factory(size=self.config.agent.storage.name)
+        elif self.config.agent.stroage.name == "gae_buffer":
+            from pytorchrl.agent.storages import GAEBuffer
+            return GAEBuffer.create_factory(size=self.config.agent.storage.size,
+                                            gae_lambda=self.config.agent.storage.gae_lambda)
         else:
             pass
     
     
+    def setup_scheme(self,):
+        params = {"algo_factory": self.algo_factory,
+                  "actor_factory": self.actor_factory,
+                  "storage_factory": self.storage_factory,
+                  "train_envs_factory": self.train_envs_factory,
+                  "test_envs_factory": self.test_envs_factory,
+                  "num_col_workers": self.config.scheme.num_col_workers,
+                  "col_workers_communication": self.config.scheme.com_col_workers,
+                  "col_workers_resources": {"num_cpus": self.config.scheme.col_workers_resources.num_cpus,
+                                            "num_gpus": self.config.scheme.col_workers_resources.num_gpus},
+                  "num_grad_workers": self.config.scheme.num_grad_workers,
+                  "grad_workers_communication": self.config.scheme.com_grad_workers,
+                  "grad_workers_resources": {"num_cpus": self.config.scheme.grad_workers_resources.num_cpus,
+                                             "num_gpus": self.config.scheme.grad_workers_resources.num_gpus},
+                  }
+        return Scheme(**params)
     
+    def setup_learner(self, ):
+        return  Learner(self.scheme,
+                        target_steps=self.config.num_env_steps, log_dir=self.config.log_dir)
     
-# might get extracted to additional utils file 
-def get_enviroment_factory(task):
-    if task == "pybullet":
-        from pytorchrl.envs.pybullet import pybullet_train_env_factory, pybullet_test_env_factory
-        return pybullet_train_env_factory, pybullet_test_env_factory
-    elif task == "atari":
-        pass
-    else:
-        pass
+    # might get extracted to additional utils file 
+    def get_enviroment_factory(self, task):
+        if task == "pybullet":
+            from pytorchrl.envs.pybullet import pybullet_train_env_factory, pybullet_test_env_factory
+            return pybullet_train_env_factory, pybullet_test_env_factory
+        elif task == "atari":
+            pass
+        else:
+            pass
         
     
 def get_algorithm(config):
@@ -88,12 +153,15 @@ def get_algorithm(config):
         return algo_factory, algo_name
     elif config.agent.name == "DDPG":
         from pytorchrl.agent.algorithms import DDPG
-        algo_factory, algo_name = DDPG.create_factory(lr_pi=config.agent.ddpg_config.lr,
-                                                      lr_q=config.agent.ddpg_config.lr,
+        algo_factory, algo_name = DDPG.create_factory(lr_pi=config.agent.ddpg_config.lr_pi,
+                                                      lr_q=config.agent.ddpg_config.lr_q,
                                                       gamma=config.agent.ddpg_config.gamma,
+                                                      max_grad_norm=config.agent.ddpg_config.max_grad_norm,
                                                       polyak=config.agent.ddpg_config.polyak,
                                                       num_updates=config.agent.ddpg_config.num_updates,
                                                       update_every=config.agent.ddpg_config.update_every,
+                                                      test_every=config.agent.ddpg_config.test_every,
+                                                      num_test_episodes=config.agent.ddpg_config.num_test_episodes,
                                                       start_steps=config.agent.ddpg_config.start_steps,
                                                       mini_batch_size=config.agent.ddpg_config.mini_batch_size)
         return algo_factory, algo_name
