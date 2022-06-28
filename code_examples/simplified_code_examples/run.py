33574c21f8b2945db8646b84f688a90dc341175d
BY571 <sebastian.dittert@gmx.de>
2022-06-28 13:15:10 +0200

    create simplified_code_examples

---
+++ code_examples/simplified_code_examples/run.py
@@ -0,0 +1,17 @@
+from omegaconf import DictConfig, OmegaConf
+import hydra
+from pytorchrl.trainer import Trainer
+
+# from pytorchrl.custom_environments import my_custom_environment_factory
+
+@hydra.main(config_name="conf", config_path="./cfg", version_base=None)
+def run_training(cfg: DictConfig) -> None:
+    config = OmegaConf.to_yaml(cfg)
+    print("Start Training\n")
+    print("Training config: ", config, "\n")
+    trainer = Trainer(cfg, custom_environment_factory=None)
+    trainer.train()
+
+
+if __name__ == "__main__":
+    run_training()
