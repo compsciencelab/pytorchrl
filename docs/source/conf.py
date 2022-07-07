fac89b775b3b859a0b4ed3816d4603a6409693c7
albert bou <albertbou92@gmail.com>
2022-07-07 12:15:18 +0200

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -60,8 +60,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-# html_theme = 'piccolo_theme'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_bernard_theme'
+html_theme_path = [sphinx_bernard_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
