80ef61d0b9c6af135c3c0bc17bfe222b96d8784a
albert bou <albertbou92@gmail.com>
2022-07-20 12:23:17 +0200

    analize results

--- code_examples/train_genchem2/ppo/analize_results.py
+++ code_examples/train_genchem2/ppo/analize_results.py
@@ -7,13 +7,15 @@ from rdkit import Chem
 from rdkit.Chem import Draw
 
 from pytorchrl.agent.env import load_baselines_results
+from code_examples.train_genchem2.ppo.train import get_args
 
 
-def analize_results(experimentpath, num_top_molecules=10):
-    """ Tries to run again failed agents in a given experiment. """
+def analize_results(num_top_molecules=10):
+
+    args = get_args()
 
     # Read monitor files
-    monitor_files = load_baselines_results(os.path.join(experimentpath, "monitor_logs/train"))
+    monitor_files = load_baselines_results(os.path.join(args.log_dir, "monitor_logs/train"))
 
     # Rank monitor files by reward
     monitor_files = monitor_files.sort_values("r", ascending=False)
@@ -25,19 +27,11 @@ def analize_results(experimentpath, num_top_molecules=10):
     smile_list = monitor_files['molecules'][:num_top_molecules].to_list()
     mols = [Chem.MolFromSmiles(smiles) for smiles in smile_list]
     image = Draw.MolsToGridImage(mols)
-    save_name = os.path.join(experimentpath, "2d_smiles.png")
+    save_name = os.path.join(args.log_dir, "2d_smiles.png")
     image.save(save_name)
     print(f"Saved images as {save_name}")
 
 
 if __name__ == "__main__":
-    import sys
-    import argparse
 
-    parser = argparse.ArgumentParser(description="RL")
-    parser.add_argument(
-        "--experimentpath", default="/tmp/genchem_ppo/",
-        help="List of directories containing experiments to be plotted")
-    args = parser.parse_args()
-
-    analize_results(experimentpath=args.experimentpath)
+    analize_results()
