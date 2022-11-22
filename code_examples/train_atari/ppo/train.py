1a75f23354d6899c0f3e8d13e1febfa01beb76d7
albert bou <albertbou92@gmail.com>
2022-11-22 16:06:39 +0100

    ppo code example wandb

--- code_examples/train_atari/ppo/train.py
+++ code_examples/train_atari/ppo/train.py
@@ -4,6 +4,7 @@ import os
 import sys
 import time
 import json
+import wandb
 import argparse
 import numpy as np
 
@@ -23,101 +24,105 @@ def main():
     cleanup_log_dir(args.log_dir)
     save_argparse(args, os.path.join(args.log_dir, "conf.yaml"), [])
 
-    info_keywords = []
-    if args.episodic_life:
-        info_keywords += ['EpisodicReward', 'Lives']
-    if args.clip_rewards:
-        info_keywords += ['UnclippedReward']
+    # Fix random seeds
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
 
-    # 1. Define Train Vector of Envs
-    train_envs_factory, action_space, obs_space = VecEnv.create_factory(
-        env_fn=atari_train_env_factory,
-        env_kwargs={
-            "env_id": args.env_id,
-            "frame_stack": args.frame_stack,
-        },
-        vec_env_size=args.num_env_processes, log_dir=args.log_dir,
-        info_keywords=tuple(info_keywords))
+    # Handle wandb init
+    if args.wandb_key:
+        mode = "online"
+        wandb.login(key=str(args.wandb_key))
+    else:
+        mode = "disabled"
 
-    # 2. Define Test Vector of Envs (Optional)
-    test_envs_factory, _, _ = VecEnv.create_factory(
-        env_fn=atari_test_env_factory,
-        env_kwargs={"env_id": args.env_id, "frame_stack": args.frame_stack},
-        vec_env_size=args.num_env_processes, log_dir=args.log_dir)
+    with wandb.init(project=args.experiment_name, name=args.agent_name, config=args, mode=mode):
 
-    # 3. Define RL training algorithm
-    algo_factory, algo_name = PPO.create_factory(
-        lr=args.lr, num_epochs=args.ppo_epoch, clip_param=args.clip_param,
-        entropy_coef=args.entropy_coef, value_loss_coef=args.value_loss_coef,
-        max_grad_norm=args.max_grad_norm, num_mini_batch=args.num_mini_batch,
-        use_clipped_value_loss=args.use_clipped_value_loss, gamma=args.gamma)
+        info_keywords = []
+        if args.episodic_life:
+            info_keywords += ['EpisodicReward', 'Lives']
+        if args.clip_rewards:
+            info_keywords += ['UnclippedReward']
 
-    # 4. Define RL Policy
-    actor_factory = OnPolicyActor.create_factory(
-        obs_space, action_space, algo_name, restart_model=args.restart_model)
+        # 1. Define Train Vector of Envs
+        train_envs_factory, action_space, obs_space = VecEnv.create_factory(
+            env_fn=atari_train_env_factory,
+            env_kwargs={
+                "env_id": args.env_id,
+                "frame_stack": args.frame_stack,
+            },
+            vec_env_size=args.num_env_processes, log_dir=args.log_dir,
+            info_keywords=tuple(info_keywords))
 
-    # 5. Define rollouts storage
-    storage_factory = GAEBuffer.create_factory(size=args.num_steps, gae_lambda=args.gae_lambda)
+        # 2. Define Test Vector of Envs (Optional)
+        test_envs_factory, _, _ = VecEnv.create_factory(
+            env_fn=atari_test_env_factory,
+            env_kwargs={"env_id": args.env_id, "frame_stack": args.frame_stack},
+            vec_env_size=args.num_env_processes, log_dir=args.log_dir)
 
-    # 6. Define scheme
-    params = {}
+        # 3. Define RL training algorithm
+        algo_factory, algo_name = PPO.create_factory(
+            lr=args.lr, num_epochs=args.ppo_epoch, clip_param=args.clip_param,
+            entropy_coef=args.entropy_coef, value_loss_coef=args.value_loss_coef,
+            max_grad_norm=args.max_grad_norm, num_mini_batch=args.num_mini_batch,
+            use_clipped_value_loss=args.use_clipped_value_loss, gamma=args.gamma)
 
-    # add core modules
-    params.update({
-        "algo_factory": algo_factory,
-        "actor_factory": actor_factory,
-        "storage_factory": storage_factory,
-        "train_envs_factory": train_envs_factory,
-        "test_envs_factory": test_envs_factory,
-    })
+        # 4. Define RL Policy
+        actor_factory = OnPolicyActor.create_factory(
+            obs_space, action_space, algo_name, restart_model=args.restart_model)
 
-    # add collection specs
-    params.update({
-        "num_col_workers": args.num_col_workers,
-        "col_workers_communication": args.com_col_workers,
-        "col_workers_resources": {"num_cpus": 1, "num_gpus": 0.5},
-    })
+        # 5. Define rollouts storage
+        storage_factory = GAEBuffer.create_factory(size=args.num_steps, gae_lambda=args.gae_lambda)
 
-    # add gradient specs
-    params.update({
-        "num_grad_workers": args.num_grad_workers,
-        "grad_workers_communication": args.com_grad_workers,
-        "grad_workers_resources": {"num_cpus": 1.0, "num_gpus": 0.5},
-    })
+        # 6. Define scheme
+        params = {}
 
-    scheme = Scheme(**params)
+        # add core modules
+        params.update({
+            "algo_factory": algo_factory,
+            "actor_factory": actor_factory,
+            "storage_factory": storage_factory,
+            "train_envs_factory": train_envs_factory,
+            "test_envs_factory": test_envs_factory,
+        })
 
-    # 7. Define learner
-    learner = Learner(scheme, target_steps=args.num_env_steps, log_dir=args.log_dir)
+        scheme = Scheme(**params)
 
-    # 8. Define train loop
-    iterations = 0
-    start_time = time.time()
+        # 7. Define learner
+        learner = Learner(scheme, target_steps=args.num_env_steps, log_dir=args.log_dir)
 
-    updates = 0
-    total_updates = np.ceil((args.num_env_steps * args.ppo_epoch * args.num_mini_batch) / (
-        args.num_env_processes * args.num_steps)) + args.ppo_epoch * args.num_mini_batch
-    alpha = np.linspace(1.0, 0.0, int(total_updates))
+        # 8. Define train loop
+        iterations = 0
+        start_time = time.time()
 
-    while not learner.done():
+        updates = 0
+        total_updates = np.ceil((args.num_env_steps * args.ppo_epoch * args.num_mini_batch) / (
+            args.num_env_processes * args.num_steps)) + args.ppo_epoch * args.num_mini_batch
+        alpha = np.linspace(1.0, 0.0, int(total_updates))
 
-        learner.step()
+        while not learner.done():
 
-        if iterations % args.log_interval == 0:
-            learner.print_info()
+            learner.step()
 
-        if iterations % args.save_interval == 0:
-            save_name = learner.save_model()
+            if iterations % args.log_interval == 0:
+                log_data = learner.get_metrics(add_episodes_metrics=True)
+                log_data = {k.split("/")[-1]: v for k, v in log_data.items()}
+                wandb.log(log_data, step=learner.num_samples_collected + args.start_env_steps)
+                learner.print_info()
 
-        if args.max_time != -1 and (time.time() - start_time) > args.max_time:
-            break
+            if iterations % args.save_interval == 0:
+                save_name = learner.save_model()
 
-        iterations += 1
-        learner.update_algorithm_parameter("lr", alpha[updates] * args.lr)
-        learner.update_algorithm_parameter("clip_param", alpha[updates] * args.clip_param)
+            if args.max_time != -1 and (time.time() - start_time) > args.max_time:
+                break
 
-    print("Finished!")
-    sys.exit()
+            iterations += 1
+            learner.update_algorithm_parameter("lr", alpha[updates] * args.lr)
+            learner.update_algorithm_parameter("clip_param", alpha[updates] * args.clip_param)
+
+        print("Finished!")
+        sys.exit()
 
 
 def get_args():
